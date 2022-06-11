197b60cf22700fbb5c0d9c686fd1bd9f4abccb66
omids0 <soheilniao@gmail.com>
2022-06-11 17:25:32 +0430

    fake api added

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
-import App from "../components/App";
+import { useEffect } from "react";
 import HomePage from "../components/HomePage";
 import Layout from "../components/Layout";
 
 export default function Home() {
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      await fetch("https://fakestoreapi.com/products")
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    };
+
+    fetchProducts()
+  }, []);
+
   return (
-    <Layout description='تست اپ جهت بررسی و آزمایش می باشد.'>
+    <Layout description="تست اپ جهت بررسی و آزمایش می باشد.">
       <HomePage />
     </Layout>
   );
