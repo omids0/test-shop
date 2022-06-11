90520d62174142a3416ac386683a8cf7a11ddb90
omids0 <soheilniao@gmail.com>
2022-06-11 18:33:01 +0430

    cards added to homepage

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,11 +1,29 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Stack } from "@mui/material";
+import { Stack, Grid, Card } from "@mui/material";
 
 export default function HomePage() {
   const allProducts = useSelector(
     (state) => state.allProductsReducer.allProducts
   );
-  console.log("Products:", allProducts.length);
-  return <Stack>HomePage</Stack>;
+
+  return (
+    <Stack p={2}>
+      {allProducts.length === 0 ? (
+        <Grid container>
+          <Grid item xs={12} sm={4} md={4}>
+            <Card sx={{ minWidth: "200px", maxWidth: "400px"}}>skeleton</Card>
+          </Grid>
+        </Grid>
+      ) : (
+        <Grid container spacing={2}>
+          {allProducts.map((item) => (
+            <Grid item xs={12} sm={4} md={4}>
+              <Card sx={{ minWidth: "200px",maxWidth: "400px" }}>{item.title}</Card>
+            </Grid>
+          ))}
+        </Grid>
+      )}
+    </Stack>
+  );
 }
