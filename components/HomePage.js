b37f9f48ee0d47212244274c5834ad04f8b20122
omids0 <soheilniao@gmail.com>
2022-06-11 18:09:50 +0430

    products in reducer tested

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { Stack } from "@mui/material";
 
 export default function HomePage() {
-  return (
-    <div>HomePage</div>
-  )
+  const allProducts = useSelector(
+    (state) => state.allProductsReducer.allProducts
+  );
+  console.log("Products:", allProducts.length);
+  return <Stack>HomePage</Stack>;
 }
