23e344ffa21279c7af1c074eecbed3f311b52a2b
omids0 <soheilniao@gmail.com>
2022-06-12 10:13:47 +0430

    home page fetch data card desined

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Stack, Grid, Card } from "@mui/material";
+import {
+  Stack,
+  Grid,
+  Card,
+  Skeleton,
+  Box,
+  CardMedia,
+  CardContent,
+  Typography,
+  Button,
+} from "@mui/material";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 export default function HomePage() {
   const allProducts = useSelector(
@@ -12,14 +23,68 @@ export default function HomePage() {
       {allProducts.length === 0 ? (
         <Grid container>
           <Grid item xs={12} sm={4} md={4}>
-            <Card sx={{ minWidth: "200px", maxWidth: "400px"}}>skeleton</Card>
+            <Box
+              sx={{
+                margin: "0 auto",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Card sx={{ width: "300px" }}>
+                <Skeleton variant="rectangular" width={"100%"} height={200} />
+                <Box p={2}>
+                  <Skeleton variant="text" height={40} />
+                  <Skeleton variant="text" />
+                  <Skeleton variant="text" />
+                </Box>
+              </Card>
+            </Box>
           </Grid>
         </Grid>
       ) : (
-        <Grid container spacing={2}>
+        <Grid container rowSpacing={8} columnSpacing={2}>
           {allProducts.map((item) => (
             <Grid item xs={12} sm={4} md={4}>
-              <Card sx={{ minWidth: "200px",maxWidth: "400px" }}>{item.title}</Card>
+              <Box
+                sx={{
+                  margin: "0 auto",
+                  display: "flex",
+                  justifyContent: "center",
+                }}
+              >
+                <Card sx={{ width: "300px", height: "650px" }}>
+                  <CardMedia
+                    component="img"
+                    height="350"
+                    image={item.image}
+                    alt="green iguana"
+                  />
+                  <CardContent
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "space-between",
+                      height: "30rem",
+                    }}
+                  >
+                    <Typography gutterBottom variant="h4" component="div">
+                      {item.title}
+                    </Typography>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        width: "100%",
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        textAlign: "center",
+                      }}
+                    >
+                      <Typography variant="h3">{`$ ${item.price}`}</Typography>
+                      <Button><AddShoppingCartIcon sx={{fontSize:'3.5rem', color: 'red'}}/></Button>
+                    </Box>
+                  </CardContent>
+                </Card>
+              </Box>
             </Grid>
           ))}
         </Grid>
